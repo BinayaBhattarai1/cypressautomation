f22c0ca84da7528e33e7a3087052b1e382897a38
bhattaraibinaya41@gmail.com <binay.bhattarai@infodevelopers.local>
2024-03-01 21:28:14 +0545

    fifth assignment

--- cypress/e2e/fifthsession.cy.js
+++ cypress/e2e/fifthsession.cy.js
@@ -1,32 +1,85 @@
-describe('CRUD Operation', () => {
-    it('Create', () => {
+// const formData= new FormData();
+// formData.append("grant_type","password");
+// formData.append("username","nagarik");
+// formData.append("password","nagarik");
+// formData.append("user_type","WEB");
+
+// describe('CRUD Operation', () => {
+//     it('Create', () => {
+//         cy.visit('http://172.31.1.20/#/auth/login'),
+//         cy.request({
+//             method: "POST",
+//             url: "http://172.31.1.20:8070/web-login",
+//             body: formData,
+//             headers: {
+//                 'Content-Type': 'multipart/form-data',
+//             }
+//         }).then((response) => {
+//             console.log("response: ",response)
+//             expect(response.status).to.eq(200)
+//             Cypress.env('Token', response.body.data.access_token)
+//             const accessToken = Cypress.env('Token')
+//             cy.log(accessToken)
+//         })
+//     })
+// })
+describe('API GET REQUEST', () => {
+    it('POST ', () => {
         cy.request({
             method: "POST",
-            url: "http://172.31.1.20/gateway/web-login",
+            url: 'https://reqres.in/api/users',
             body: {
-                grant_type: "password",
-                username: "nagarik",
-                password: "nagarik",
-                user_type: "WEB"
-            },
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Connection': 'keep-alive',
-                'Content-Length': '452',
-                'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryXzi0AZaFQIje7bQ6',
-                'Host': '172.31.1.20',
-                'Origin': 'http://172.31.1.20:9999',
-                'Referer': 'http://172.31.1.20:9999/',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+                name: "Binaya Bhattarai",
+                job: "QA",
             }
         }).then((response) => {
-            console.log(response)
-            expect(response.status).to.eq(200)
-            Cypress.env('Token', response.body.data.access_token)
-            const accessToken = Cypress.env('Token')
-            cy.log(accessToken)
+            expect(response.status).to.eq(201)
+            expect(response.body.name).to.eq('Binaya Bhattarai')
+            expect(response.body.job).to.eq('QA')
         })
+    });
+});
+
+it('get users', () => {
+    cy.request({
+        method: "GET",
+        url: "https://reqres.in/api/users/2",
+    }).then((response) => {
+        console.log(response)
+        Cypress.env('firstname', response.body.data.first_name)
+        const firstName = Cypress.env('firstname')
+        cy.log(firstName)
     })
 })
+
+
+it('POST Request', () => {
+    const firstName = Cypress.env('fName ')
+    cy.request({
+        method: "POST ",
+        url: 'https://reqres.in/api/users',
+        body: {
+            "name": firstName,
+            "job": "QA ",
+        }
+    })
+        .then((response) => {
+            expect(response.status).to.eq(201)
+            expect(response.body.name).to.eq('Janet')
+            expect(response.body.job).to.eq('QA')
+        })
+});
+
+let fullname;
+it('get users', () => {
+    cy.request({
+        method: "GET",
+        url: "https://reqres.in/api/users/2",
+    }).then((response) => {
+        console.log(response)
+        Cypress.env('fName', response.body.data.first_name)
+        fullname = Cypress.env('fName')
+        cy.log(fullname)
+    })
+})
+
